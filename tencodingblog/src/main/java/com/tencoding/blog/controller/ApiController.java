78ae96db9457857dcb479a097a01a941c6153a02
ji_euni <ji_euni@naver.com>
2022-12-07 21:40:57 +0900

    first commit (세팅완료)

---
+++ tencodingblog/src/main/java/com/tencoding/blog/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.tencoding.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class ApiController {
+
+	@GetMapping("/hello")
+	public String get () {
+		return "test1";
+	}
+}
+ 
