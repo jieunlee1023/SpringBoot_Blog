b285d2173e82542179142bb95dc7ea1469e95e71
ji_euni <ji_euni@naver.com>
2022-12-15 16:23:18 +0900

    카카오 토큰 받기

--- tencodingblog/src/main/java/com/tencoding/blog/controller/UserController.java
+++ tencodingblog/src/main/java/com/tencoding/blog/controller/UserController.java
@@ -3,34 +3,79 @@ package com.tencoding.blog.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 //@RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired
-	 private HttpSession session;
+	private HttpSession session;
 
 	@GetMapping("/auth/login_form")
 	public String loginForm() {
-		//경로 : /WEB-INF/views/user/login_form.jsp;
+		// 경로 : /WEB-INF/views/user/login_form.jsp;
 		return "user/login_form";
 	}
-	
+
 	@GetMapping("/auth/join_form")
 	public String joinForm() {
 		return "user/join_form";
 	}
-	
+
 	@GetMapping("/user/update_form")
 	public String updateForm() {
 		return "user/update_form";
 	}
-	
-	
+
+//	POST /oauth/token HTTP/1.1
+//	Host: kauth.kakao.com
+//	Content-type: application/x-www-form-urlencoded;charset=utf-8
+
+	// https://kauth.kakao.com/oauth/token
+
+	// 페이지에서 데이터를 리턴하는 방법
+	@GetMapping("/auth/kakao/callback")
+	@ResponseBody // data를 리턴함
+	public String kakaoCallback(@RequestParam String code) {
+		// 여기서  카카오 서버에서 보내 준 code 값을 받을 수 있다.
+		// 다음 단계는 토큰 발급 받기
+		
+		RestTemplate rt = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		// 바디 만들기
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "a3af89fb8269257123c6854ffafc500d");
+		params.add("redirect_uri", "http://localhost:9090/auth/kakao/callback");
+		params.add("code", code);
+		
+		HttpEntity<MultiValueMap<String, String>> requestKakaoToken 
+		= new HttpEntity<>(params, headers);
+		
+		//헤더 변조 해서 실행 시키는 메서드는 RestTemplate exchange() 이다
+		ResponseEntity<String> response = rt.exchange("https://kauth.kakao.com/oauth/token", 
+				HttpMethod.POST,
+				requestKakaoToken,
+				String.class);
+		
+		
+		return "카카오 토큰 코드 : \n" + response;
+	}
+
 // 기존 스프링에서 로그아웃 처리는 따로 정리 !
 //	@GetMapping("/logout")
 //    public String logout() {
@@ -40,5 +85,5 @@ public class UserController {
 //
 //        return "redirect:/";
 //    }
-	
+
 }
--- tencodingblog/src/main/webapp/WEB-INF/views/user/login_form.jsp
+++ tencodingblog/src/main/webapp/WEB-INF/views/user/login_form.jsp
@@ -6,18 +6,16 @@
 <div class="container">
 	<form action="/auth/loginProc" method="post">
 		<div class="form-group">
-			<label for="username">username:</label> 
-			<input type="text" 
-			class="form-control" placeholder="Enter username"
-			 id="username" name="username" value="teco">
+			<label for="username">username:</label> <input type="text" class="form-control" placeholder="Enter username" id="username" name="username"
+				value="teco">
 		</div>
 		<div class="form-group">
-			<label for="password">password:</label> 
-			<input type="password"
-			 class="form-control" placeholder="Enter password" 
-			id="password" name="password" value="asd1234">
+			<label for="password">password:</label> <input type="password" class="form-control" placeholder="Enter password" id="password" name="password"
+				value="asd1234">
 		</div>
 		<button type="submit" id="btn--login" class="btn btn-primary">signIn</button>
+		<a href="https://kauth.kakao.com/oauth/authorize?client_id=a3af89fb8269257123c6854ffafc500d&redirect_uri=http://localhost:9090/auth/kakao/callback&response_type=code">
+			<img alt="카카오로그인" src="/image/kakao_login.png" width="70" height="40"></a>
 	</form>
 </div>
 
